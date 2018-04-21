decea243a3df715b9c9a21cf50535df600bbc8b2
Jcweeden <Joshua@Joshuas-MacBook-Pro.local>
2018-04-21 16:08:42 +0100

    New Button Input.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour {
 	private float xAxis, yAxis;
 
 	public float movementSpeed = 7;
+	public float rotationSpeed = 2;
 
 
 	// Use this for initialization
@@ -20,19 +21,36 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		xAxis = XboxCtrlrInput.XCI.GetAxis (XboxCtrlrInput.XboxAxis.LeftStickX);
+//		xAxis = XboxCtrlrInput.XCI.GetAxis (XboxCtrlrInput.XboxAxis.LeftStickX) * movementSpeed;
 
-		yAxis = XboxCtrlrInput.XCI.GetAxis (XboxCtrlrInput.XboxAxis.LeftStickY);
 
-		Debug.Log("xAxis: " + xAxis);
-		Debug.Log("yAxis: " + yAxis);
 
+
+		float rightTriggerPressed = XboxCtrlrInput.XCI.GetAxis (XboxCtrlrInput.XboxAxis.RightTrigger);
+		float leftTriggerPressed = XboxCtrlrInput.XCI.GetAxis (XboxCtrlrInput.XboxAxis.LeftTrigger);
+
+		if (leftTriggerPressed == 1f) {
+			xAxis = 1 * movementSpeed;
+		} else {
+			xAxis = 0;
+		}
+
+		yAxis = XboxCtrlrInput.XCI.GetAxis (XboxCtrlrInput.XboxAxis.LeftStickY) * rotationSpeed;
+
+		Debug.Log("xAxis: " + xAxis + " yAxis: " + yAxis + " rightTrigger: " + rightTriggerPressed + " leftTrigger: " + leftTriggerPressed + " Y: " + XboxCtrlrInput.XCI.GetButtonDown (XboxCtrlrInput.XboxButton.Y));
+//		Debug.Log();
 	}
 
 	private void FixedUpdate() {
-		this.rigidBody.velocity = new Vector2(xAxis, -yAxis);
+//		this.rigidBody.velocity = new Vector3(xAxis, 0,  0);
+//		rigidBody.AddForce (new Vector3(transform.forward.x, 0 , transform.forward.z) * xAxis);
+//		this.gameObject.transform.Rotate (new Vector3(yAxis,0,0));
+	
+		Vector3 movement = transform.rotation * Vector3.forward;	//calculate movement direction by rotation
+
+		rigidBody.AddForce (xAxis  * Time.deltaTime * movement, ForceMode.Impulse);	
+
+		transform.Rotate (Vector3.up, yAxis * Time.deltaTime);	//steering
 
 	}
-
-
 }
