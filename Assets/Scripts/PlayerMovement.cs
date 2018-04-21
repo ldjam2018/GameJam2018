bcc060429c0231b4ec10ca0048e8ab18cafc19aa
Jcweeden <Joshua@Joshuas-MacBook-Pro.local>
2018-04-21 12:49:45 +0100

    Controller Input, Player Movement.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+
+	private float xAxis, yAxis;
+
+	public float movementSpeed = 7;
+
+
+	// Use this for initialization
+	void Start () {
+		this.rigidBody = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		xAxis = XboxCtrlrInput.XCI.GetAxis (XboxCtrlrInput.XboxAxis.LeftStickX);
+
+		yAxis = XboxCtrlrInput.XCI.GetAxis (XboxCtrlrInput.XboxAxis.LeftStickY);
+
+		Debug.Log("xAxis: " + xAxis);
+		Debug.Log("yAxis: " + yAxis);
+
+	}
+
+	private void FixedUpdate() {
+		this.rigidBody.velocity = new Vector2(xAxis, -yAxis);
+
+	}
+
+
+}
